520ebb8113d872018d68be73cbd5fc4b0ce1277f
dglazyrin <dglazyrin@gmail.com>
2023-07-06 22:13:28 +0300

    Homework 3

---
+++ HomeWork 3/2.js
@@ -0,0 +1,6 @@
+function hello(n1) {
+    alert(`Ну, здравствуй, ${n1}`);
+}
+
+let hisName = prompt("Вы, собственно, кто?");
+hello(hisName)
---
+++ HomeWork 3/main.js
@@ -0,0 +1,3 @@
+let Cel = prompt("Сколько там сегодня?");
+let Fahr = Math.round(Cel*1.8+32);
+alert(`Цельсий ${Cel}, Фаренгейт ${Fahr}`);
